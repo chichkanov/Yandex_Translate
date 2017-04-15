677bdf42cbaec20d1c93ee55532b55696508561d
chichkanov <agchichkanov@gmail.com>
2017-04-15 23:01:23 +0300

    new card ui

--- app/src/main/java/com/chichkanov/yandex_translate/SwitchLanguageView.java
+++ app/src/main/java/com/chichkanov/yandex_translate/SwitchLanguageView.java
@@ -4,6 +4,7 @@ package com.chichkanov.yandex_translate;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
@@ -28,5 +29,12 @@ public class SwitchLanguageView extends RelativeLayout{
         spinnerFrom = (Spinner) findViewById(R.id.spinner_translate_from);
         spinnerTo = (Spinner) findViewById(R.id.spinner_translate_to);
         switchLanguage = (ImageButton) findViewById(R.id.ib_translate_switch);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.languages_array, R.layout.spinner_item_main);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFrom.setAdapter(adapter);
+        spinnerTo.setAdapter(adapter);
+        spinnerFrom.setSelection(0);
+        spinnerTo.setSelection(1);
     }
 }
